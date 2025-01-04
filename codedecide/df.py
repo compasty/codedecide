2dd45c0357eda326481757cede1b710ff9d3c27d
compasty <compasty@163.com>
2025-01-05 00:03:02 +0800

    feat: 完善数据读取

--- codedecide/df.py
+++ codedecide/df.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 
 def read_feather_file(file_path, top_n: int = -1, date_col: str = 'date'):
@@ -10,6 +11,23 @@ def read_feather_file(file_path, top_n: int = -1, date_col: str = 'date'):
     if top_n > 0:
         df = df.head(top_n)
     return df
+
+
+def convert_symbol(symbol: str = ''):
+    return symbol.replace('/', '_').replace(':', '_')
+
+
+def read_data(exchange: str = 'binance', trading_mode: str = 'futures', symbol: str = 'BTC/USDT:USDT', timeframe: str = '1d', format: str = 'feather'):
+    home_path = os.environ['CRYPTO_DATA_HOME']
+    assert home_path is not None
+    data_dir = os.path.join(home_path, exchange, trading_mode)
+    file_name = convert_symbol(symbol) + '-' + \
+        timeframe + '-' + trading_mode + '.' + format
+    file_path = os.path.join(data_dir, file_name)
+    if format == 'feather':
+        return read_feather_file(file_path)
+    else:
+        return None
 
 
 def merge_bars(df: pd.DataFrame, timeframe: str):
--- tests/test_indicators.py
+++ tests/test_indicators.py
@@ -1,3 +1,4 @@
+import os
 import unittest
 
 from codedecide import IndicatorPlotConfig
@@ -9,6 +10,10 @@ class TestIndicators(unittest.TestCase):
         self.assertEqual(config1.col_name, 'r1')
         self.assertIsNone(config1.legend_group)
         self.assertTrue(config1.separate)
-        config2 = IndicatorPlotConfig(col_name='r2', trace_name='r2', separate=False)
+        config2 = IndicatorPlotConfig(
+            col_name='r2', trace_name='r2', separate=False)
         self.assertEqual(config2.col_name, 'r2')
         self.assertFalse(config2.separate)
+
+    def test_os_env(self) -> None:
+        self.assertIsNone(os.environ['NOT_EXIST'])
